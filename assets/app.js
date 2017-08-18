f8fbb8e066672da135cd118e7f7994e343d0d6c0
Clement-R <rondeau.clement@gmail.com>
2017-08-18 16:17:16 +0200

    Start from scratch, hompage first

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+
+/*
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+
+  	var canvasElement = document.getElementsByClassName("particles-js-canvas-el");
+	for (var i=0; i<canvasElement.length; i++) {
+	  canvasElement[i].style.height = "50%";
+	  canvasElement[i].height(0);
+	}
+});
+*/
